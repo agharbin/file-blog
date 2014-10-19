f304814aa997b867f48f1c2876fc21a03d6f3cb9
Adam Harbin <harbin.adam@gmail.com>
2014-10-18 22:02:05 -0500

    Adding template skeleton

--- blogServer.js
+++ blogServer.js
@@ -1,14 +1,15 @@
-var http = require("http");
-var url = require("url");
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 function run() {
 	function requestHandler(request, response){
-		response.writeHead(200, {"Content-Type": "text/html"});
-		response.write("<!DOCTYPE HTML>");
-		response.write("<html>");
-		response.write("<head><title>Title</title></head>");
-		response.write("<body><p>Body</p></body>");
-		response.write("</html>");
+		fs.readFile('./templates/template.html', function(err,data){
+			if (err) throw err;
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.write(data);
+			response.end();
+		});
 	}
 	http.createServer(requestHandler).listen(80);
 }
